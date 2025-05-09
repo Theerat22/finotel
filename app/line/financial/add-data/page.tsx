98394b913fa16c332042c43bba9549de94b4bc67
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2025-05-09 16:27:32 +0700

    change input style

--- app/line/financial/add-data/page.tsx
+++ app/line/financial/add-data/page.tsx
@@ -635,7 +635,7 @@ const HotelFinanceForm = () => {
                         type="file"
                         accept="image/*"
                         onChange={handleFileChange}
-                        className="w-full border border-gray-300 rounded p-2 text-sm"
+                        className="w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-medium file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
                       />
                     </div>
                   </div>
@@ -644,7 +644,7 @@ const HotelFinanceForm = () => {
                     disabled={loading || !image}
                     className={`w-full py-2 px-4 mt-4 rounded font-medium  ${
                       loading || !image
-                        ? "bg-gray-400 cursor-not-allowed"
+                        ? "bg-gray-400 text-white font-bold cursor-not-allowed"
                         : "bg-blue-600 text-white font-bold hover:bg-blue-700"
                     }`}
                   >
